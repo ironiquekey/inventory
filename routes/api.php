6bb74ec2809c744d513126a3362a9b71a9169908
maktezi <mlcnierras@gmail.com>
2024-06-23 11:55:46 +0800

    fix: multiple files

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,14 +9,11 @@ use App\Http\Controllers\Api\AuthController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/signup',[AuthController::class, 'signup']);
-Route::post('/login',[AuthController::class, 'login']);
